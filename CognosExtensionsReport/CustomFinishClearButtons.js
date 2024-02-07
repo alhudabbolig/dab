f8b4eae7628ba6f984aa50930fb0ca46a4154fb1
alhudabbolig <159250697+alhudabbolig@users.noreply.github.com>
2024-02-07 11:21:36 +0100

    Add files via upload

---
+++ CognosExtensionsReport/CustomFinishClearButtons.js
@@ -0,0 +1,46 @@
+define(function () {
+	"use strict";
+
+	function validatePromptValue() {
+	};
+
+
+	validatePromptValue.prototype.draw = function (oControlHost) {
+
+		var el = oControlHost.container;
+		el.innerHTML =
+		'<style>' +
+			'.myButton { height:32px; width:120px; cursor:pointer; margin-left:10px; color:#4178BE; font-size:14px; padding:6px 12px 6px 12px; background-color:white; border:1px solid #4178BE; }' +
+			'.myButton:hover { background-color:#4178BE; color:white; border:1px solid #4178BE; }' +
+		'</style>' +
+		'<button class="myButton btnFinish" type="button">Udfør</button>' +
+		'<button class="myButton btnClear" type="button">Ryd</button>';
+
+		el.querySelector(".btnClear").onclick = this.f_clearButtonClick.bind(this, oControlHost);
+		el.querySelector(".btnFinish").onclick = this.finishButtonClick.bind(this, oControlHost);
+	};
+
+
+	validatePromptValue.prototype.finishButtonClick = function (oControlHost) {
+
+		oControlHost.finish();
+
+	};
+
+
+	validatePromptValue.prototype.f_clearButtonClick = function (oControlHost) {
+
+		var aControls = oControlHost.page.getAllPromptControls();
+
+		for (var i = 0; i < aControls.length; i++) {
+			var oControl = aControls[i];
+			oControl.clearValues();
+            
+		}
+
+        oControlHost.finish();
+
+	};
+
+	return validatePromptValue;
+});
